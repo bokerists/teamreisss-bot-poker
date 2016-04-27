1dc990014943ac3269cb46f8ea5c51f5330387cf
Samuele Resca <samuele.resca@gmail.com>
2016-04-27 19:21:16 +0200

    first commit

--- player.js
+++ player.js
@@ -3,15 +3,24 @@ exports = module.exports = {
 
   VERSION: 'Superstar poker js-player',
 
-  bet: function (gamestate) {
+  checkCoppia: function (a, b) {
+        return a == b;
+    },
 
+
+  bet: function (gamestate) {
+        //Questo sono io
+       var player= gamestate.players[gamestate.me];
+        var call = gamestate.callAmount
     //
     // gamestate contains info about the state of the game.
     // check the documentation at https://bot-poker.herokuapp.com/wiki#gamestate for further info about the data structure.
 
     //
     // you just have to return the amount that you want to bet.
-
+      if (this.checkCoppia(gamestate.players[me].cards[0].rank, gamestate.players[me].cards[1].rank)) {
+        return call *2;
+      }
 
 
     // enjoy the game!
